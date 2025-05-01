901a64cba1b01e7c61a6b40f2e536408114b6d2d
iprince <imprince0712@gmail.com>
2025-05-01 19:13:42 +0930

    changed controllerwithviews

--- Server/Program.cs
+++ Server/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // added comment to delete it from git
 // why program.cs is not appearing 
-builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
