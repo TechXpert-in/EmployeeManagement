0325e07a304e89b5f8235ca7e7da2aa0cdebde58
iprince <imprince0712@gmail.com>
2025-05-01 19:15:22 +0930

    changed swaggerui in program.cs

--- Server/Program.cs
+++ Server/Program.cs
@@ -28,7 +28,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty; // Set the Swagger UI at the app's root
+    });
 }
 
 app.UseSwagger();
